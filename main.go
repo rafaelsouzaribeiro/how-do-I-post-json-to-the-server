ad0736bfa1a7ef3ad300a5ae3184dd088c4878af
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2023-12-08 00:27:15 -0300

    my commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Client struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Work      string `json:"work"`
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allow", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var client Client
+
+	// Retorna um slice de bytes ([]byte, error)
+	err := json.NewDecoder(r.Body).Decode(&client)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Pega struct e convert para json
+	jsonParse, err := json.Marshal(client)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonParse)
+
+}
